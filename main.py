14c13e4bc7edf84adc207fb6ce054d6f884b8e0d
Loc <psbalhester@gmail.com>
2022-07-05 22:16:47 -0300

    Exercicio Executado

---
+++ main.py
@@ -0,0 +1,41 @@
+# exercicio_01
+"""
+Programa em Python que solicite as informações de três pessoas, como nome, idade, peso, altura.
+Apresenta na tela estas informações de modo que permaneçam alinhados verticalmente com formatação de interpolação.
+"""
+nome1 = nome2 = nome3 = ''
+peso1 = peso2 = peso3 = 0.0
+altura1 = altura2 = altura3 = 0.0
+idade1 = idade2 = idade3 = 0
+
+nome1 = input('Entre com o primeiro nome: ')
+idade1 = input('Entre com a primeira idade: ')
+peso1 = input('Entre com o primeiro peso: ')
+altura1 = input('Entre com a primeira altura: ')
+
+nome2 = input('Entre com o segundo nome: ')
+idade2 = input('Entre com a segunda idade: ')
+peso2 = input('Entre com o segundo peso: ')
+altura2 = input('Entre com a segunda altura: ')
+
+nome3 = input('Entre com o terceiro nome: ')
+idade3 = input('Entre com a terceira idade: ')
+peso3 = input('Entre com o terceiro peso: ')
+altura3 = input('Entre com a terceira altura: ')
+
+
+print(f"Primeiro Nome: {nome1}")
+print(f"Primeira Idade: {idade1}")
+print(f"Primeiro Peso: {peso1}")
+print(f"Primeira Altura: {altura1}")
+
+print(f"Segundo Nome: {nome2}")
+print(f"Segunda Idade: {idade2}")
+print(f"Segundo Peso: {peso2}")
+print(f"Segunda Altura: {altura2}")
+
+print(f"Terceiro Nome: {nome3}")
+print(f"Terceira Idade: {idade3}")
+print(f"Terceiro Peso: {peso3}")
+print(f"Terceira Altura: {altura3}")
+
